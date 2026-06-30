4fb88c58564d8c9d4eadc815c572ab2a7eb11865
as0294995-gif <as0294995@gmail.com>
2026-06-30 21:11:14 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,64 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const products = document.querySelectorAll(".product");
-    const invoiceBody = document.getElementById("invoiceBody");
-    const totalPrice = document.getElementById("totalPrice");
+const products = document.querySelectorAll(".product");
+const invoiceBody = document.getElementById("invoiceBody");
+const totalPrice = document.getElementById("totalPrice");
 
-    let total = 0;
+let total = 0;
+let cart = {};
 
-    products.forEach(function (product) {
+products.forEach(function(product){
 
-        product.addEventListener("click", function () {
+product.addEventListener("click",function(){
 
-            const name = this.textContent;
-            const price = 10;
+const name=this.textContent;
+const price=10;
 
-            const row = document.createElement("tr");
+if(cart[name]){
 
-            row.innerHTML = `
-                <td>${name}</td>
-                <td>1</td>
-                <td>${price}</td>
-                <td>${price}</td>
-            `;
+cart[name].qty++;
+cart[name].total=cart[name].qty*price;
 
-            invoiceBody.appendChild(row);
+}else{
 
-            total += price;
-            totalPrice.textContent = total.toFixed(2);
+cart[name]={
+qty:1,
+price:price,
+total:price
+};
 
-        });
+}
 
-    });
+drawInvoice();
+
+});
+
+});
+
+function drawInvoice(){
+
+invoiceBody.innerHTML="";
+
+total=0;
+for(let item in cart){
+
+const row=document.createElement("tr");
+
+row.innerHTML=`
+<td>${item}</td>
+<td>${cart[item].qty}</td>
+<td>${cart[item].price}</td>
+<td>${cart[item].total}</td>
+`;
+
+invoiceBody.appendChild(row);
+
+total+=cart[item].total;
+
+}
+
+totalPrice.textContent=total.toFixed(2);
+
+}
 
 });
